15175a7609c7dff4f9ca058690590b21b3390a51
yonnie-park <juon0805@gmail.com>
2022-11-21 23:14:40 +0900

    add state, css

--- harudiary/src/Pages/Landing.js
+++ harudiary/src/Pages/Landing.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 import "./Landing.css"
-const Landing = () => {
+export default function Landing() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    function validateForm(){
+        return username.length>0 && password.length>0
+    }
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log("submit")
+    }
     return(
-       <div>
+       <div className="login">
         <div id="title">
-            <h1>HARU DIARY</h1>
+            <p>haru diary</p>
+            <div id="title_highlighter"></div>
         </div>
         <div id="fieldset">
-            <p>username</p>
-            <input id="username"></input>
-            <p>password</p>
-            <input id="password"></input> <br/><br/>
-            <button id="signin"> sign in </button><br/><br/>
-            <button id="signup"> Sign up</button><br/><br/>
-            <button id="forgot">Forgot your password?</button>
+            <Form onSubmit={handleSubmit}>
+            <div id="border">
+                <Form.Group size="lg" controlId="username">
+                    <Form.Control
+                        placeholder="username"
+                        id="formbox" 
+                        autoFocus 
+                        type="username"
+                        value={username} 
+                        onChange={(e)=>setUsername(e.target.value)} />
+                </Form.Group>
+            </div>
+            <div id="border-1">
+                <Form.Group size="lg" controlId="password">
+                    <Form.Control
+                        placeholder="password"
+                        id="formbox" 
+                        type="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}/>
+                </Form.Group>
+            </div>    
+
+                <Button 
+                    id="loginBTN" size="lg" type="submit" disabled={!validateForm()}>
+                    continue
+                    <div id="highlighter"></div>
+                </Button>
+            </Form>
         </div>
        </div> 
     )
 }
-export default Landing;
