9fa009d1cd0ae32ec45ee7920fb10f5d7d0f5f64
yonnie-park <juon0805@gmail.com>
2022-11-24 20:56:48 +0900

    add calendar

--- client/src/Pages/MyPage.js
+++ client/src/Pages/MyPage.js
@@ -1,20 +1,27 @@
 import React from "react";
 import "./MyPage.css"
 import axios from "axios"
+import Calendar from 'react-calendar'; 
+import {useState} from 'react';
 
 axios.defaults.withCredentials = true
 
-const MyPage = (props) => {
-    if(props.userInfo){
+const MyPage = () => {
+    const [date, setDate] = useState(new Date())
         return(
             <div id="mypage">
-                <div id="title">
-                    <p>{props.userInfo.username}</p>
-                    <div id="title_highlighter"></div>
+                <div id="mypage_title">
+                    <p>{date.toDateString()}</p>
+                    <div id="mypage_title_highlighter"></div>
+                </div> <br></br>
+                <div id="row">
+                    <div className="col-lg-8" id="calendar">
+                    <Calendar onChange={setDate} value={date}/>
+                    </div>
+                    <div className="col-lg-4" id="todo"></div>
                 </div>
             </div> 
          )
     }
     
-}
 export default MyPage;
